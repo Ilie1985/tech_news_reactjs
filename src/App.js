3f4df7203de1202c21a6922cc0491ff38c2201f5
Ilie1985 <ilie.marian32@yahoo.com>
2022-12-13 16:12:01 +0000

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import SearchForm from './SearchForm'
-import Stories from './Stories'
-import Buttons from './Buttons'
+import React, { Fragment } from "react";
+import SearchForm from "./SearchForm";
+import Stories from "./Stories";
+import Buttons from "./Buttons";
+
 function App() {
-  return <h2>hacker news starter</h2>
+  return (
+    <Fragment>
+      <SearchForm />
+      <Buttons />
+      <Stories />
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import { useGlobalContext } from './context'
+import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  return <h2>stories component</h2>
+  const { isLoading } = useGlobalContext();
+
+
+if(isLoading){
+return <div className="loading"></div>
 }
 
-export default Stories
+
+
+
+
+  return <h2>stories component</h2>;
+};
+
+export default Stories;
